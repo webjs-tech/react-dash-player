c6192b354b94ba5ef198716089cab4c77c641d9e
Eduard Prystupa <129734981+EdPrys@users.noreply.github.com>
2023-07-06 17:34:17 +0300

    Update components/shared/Icon/Icon.tsx
    got it
    
    Co-authored-by: Artem Shcherbakov <Artem2431@gmail.com>

--- components/shared/Icon/Icon.tsx
+++ components/shared/Icon/Icon.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classNames from "classnames";
 
-import style from "./Icon.module.scss";
+import styles from "./Icon.module.scss";
 
 type VariantType =
   | "information"
