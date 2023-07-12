166b0ec6fd9a19d18e5830ef4b0aba077b119234
EdPrys <eduard.prystupa@gmail.com>
2023-07-12 14:59:20 +0300

    complate: first version of player

---
+++ components/VideoPlayer/VideoPlayer.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef } from 'react';
+import shaka from 'shaka-player/dist/shaka-player.ui';
+import 'shaka-player/dist/controls.css';
+
+type Props = {
+  videoUrl: string;
+};
+
+const VideoPlayer: React.FC<Props> = ({ videoUrl }) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const initPlayer = async () => {
+      try {
+        await shaka.polyfill.installAll();
+        const player = new shaka.Player(videoRef.current);
+
+        player.configure({
+          controlPanelElements: [],
+        });
+
+        await player.load(videoUrl);
+      } catch (error) {
+        console.error('error', error);
+      }
+    };
+
+    initPlayer();
+  }, [videoUrl]);
+
+  return (
+    <div>
+      <video ref={videoRef} controls={false}></video>
+    </div>
+  );
+};
+
+export default VideoPlayer;
