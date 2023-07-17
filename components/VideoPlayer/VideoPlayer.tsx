c33ee4028bac81481bbb0b44917ebfced3ea7dc3
EdPrys <eduard.prystupa@gmail.com>
2023-07-17 12:58:41 +0300

    add: events on videoPlayer

--- components/VideoPlayer/VideoPlayer.tsx
+++ components/VideoPlayer/VideoPlayer.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import shaka from 'shaka-player/dist/shaka-player.ui';
 import 'shaka-player/dist/controls.css';
+import styles from './VideoPlayer.module.scss';
 
 type Props = {
   videoUrl: string;
@@ -8,6 +9,8 @@ type Props = {
 
 const VideoPlayer: React.FC<Props> = ({ videoUrl }) => {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const previewRef = useRef<HTMLCanvasElement>(null);
+  const [currentTime, setCurrentTime] = useState(0);
 
   useEffect(() => {
     const initPlayer = async () => {
@@ -28,9 +31,32 @@ const VideoPlayer: React.FC<Props> = ({ videoUrl }) => {
     initPlayer();
   }, [videoUrl]);
 
+  const timeFormat = (currentTime: number) => {
+    // Implement the timeFormat function for time formatting
+    // based on the currentTime value
+    // return `${Math.floor(currentTime / 60)}:${currentTime % 60}`;
+    return '';
+  };
+
+  const handleMouseMove = (
+    event: any /* most biggest issue: what event should be here? */
+  ) => {
+    // event processing currentTime
+  };
+
   return (
-    <div>
-      <video ref={videoRef} controls={true}></video>
+    <div className={styles.videoContainer}>
+      <div className={styles.previewСontainer} onMouseMove={handleMouseMove}>
+        <canvas ref={previewRef} className={styles.canvas} />
+        <span className={styles.previewTime}>{timeFormat(currentTime)}</span>
+      </div>
+      <video
+        ref={videoRef}
+        controls={true}
+        style={{ width: '450px', height: 'auto' }}
+        onMouseMoveCapture={handleMouseMove}
+        className={styles.videoPlayer}
+      ></video>
     </div>
   );
 };
