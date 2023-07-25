df31141266f381cdbb2c1d974fa4962aae01a299
EdPrys <eduard.prystupa@gmail.com>
2023-07-25 14:20:46 +0300

    add: hoverTime, move div

--- components/VideoPlayer/VideoPlayer.tsx
+++ components/VideoPlayer/VideoPlayer.tsx
@@ -10,7 +10,9 @@ type Props = {
 const VideoPlayer: React.FC<Props> = ({ videoUrl }) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const previewRef = useRef<HTMLCanvasElement>(null);
-  const [currentTime, setCurrentTime] = useState(0);
+  const [hoveredTime, setHoveredTime] = useState(0);
+  const [isHovering, setIsHovering] = useState(false);
+  const [canvasX, setCanvasX] = useState(0);
 
   useEffect(() => {
     const initPlayer = async () => {
@@ -31,31 +33,60 @@ const VideoPlayer: React.FC<Props> = ({ videoUrl }) => {
     initPlayer();
   }, [videoUrl]);
 
-  const timeFormat = (currentTime: number) => {
-    // Implement the timeFormat function for time formatting
-    // based on the currentTime value
-    // return `${Math.floor(currentTime / 60)}:${currentTime % 60}`;
-    return '';
+  const timeFormat = (time: number) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes.toString().padStart(2, '0')}:${seconds
+      .toString()
+      .padStart(2, '0')}`;
   };
 
-  const handleMouseMove = (
-    event: any /* most biggest issue: what event should be here? */
-  ) => {
-    // event processing currentTime
+  const handleVideoMouseMove = (event: React.MouseEvent<HTMLVideoElement>) => {
+    const videoElement = event.currentTarget;
+    const rect = videoElement.getBoundingClientRect();
+    const offsetX = event.nativeEvent.offsetX;
+    const totalWidth = rect.width;
+    const percentage = offsetX / totalWidth;
+    const videoDuration = videoElement.duration;
+    const time = videoDuration * percentage;
+    setHoveredTime(time);
+    setIsHovering(true);
+
+    const canvasWidth = 100;
+    const canvasHeight = 50;
+    const canvasX = event.pageX - rect.left - canvasWidth / 2;
+    setCanvasX(canvasX);
+    const canvasY = event.pageY - rect.top - rect.height - canvasHeight - 10;
+    previewRef.current!.style.left = `${canvasX}px`;
+    previewRef.current!.style.top = `${canvasY}px`;
   };
 
   return (
     <div className={styles.videoContainer}>
-      <div className={styles.previewСontainer} onMouseMove={handleMouseMove}>
-        <canvas ref={previewRef} className={styles.canvas} />
-        <span className={styles.previewTime}>{timeFormat(currentTime)}</span>
+      <div
+        className={styles.previewСontainer}
+        style={{
+          display: isHovering ? 'flex' : 'none',
+          left: `${canvasX + 50}px`,
+          top: 130 + 'px',
+        }}
+      >
+        <canvas
+          ref={previewRef}
+          width={100}
+          height={50}
+          className={styles.canvas}
+        />
+        <span className={styles.previewTime}>{timeFormat(hoveredTime)}</span>
       </div>
       <video
         ref={videoRef}
         controls={true}
-        style={{ width: '450px', height: 'auto' }}
-        onMouseMoveCapture={handleMouseMove}
+        style={{ width: '600px', height: 'auto' }}
         className={styles.videoPlayer}
+        onMouseMove={handleVideoMouseMove}
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
       ></video>
     </div>
   );
