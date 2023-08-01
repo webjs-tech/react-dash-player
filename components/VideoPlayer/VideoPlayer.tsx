34ba6034cc9547490610eb8cd882c35b6ccda57f
EdPrys <eduard.prystupa@gmail.com>
2023-08-01 15:40:15 +0300

    no-edit

--- components/VideoPlayer/VideoPlayer.tsx
+++ components/VideoPlayer/VideoPlayer.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import shaka from 'shaka-player/dist/shaka-player.ui';
+
 import 'shaka-player/dist/controls.css';
 import styles from './VideoPlayer.module.scss';
 
@@ -9,9 +10,39 @@ type Props = {
 
 const VideoPlayer: React.FC<Props> = ({ videoUrl }) => {
   const videoRef = useRef<HTMLVideoElement>(null);
-  const videoContainerRef = useRef<HTMLDivElement>(null);
   const previewRef = useRef<HTMLCanvasElement>(null);
-  const [videoDuration, setVideoDuration] = useState(0);
+  const videoContainerRef = useRef<HTMLDivElement>(null);
+  const [hoveredTime, setHoveredTime] = useState(0);
+  const [isHovering, setIsHovering] = useState(false);
+  const [canvasX, setCanvasX] = useState(0);
+
+  // useEffect(() => {
+  //   const initPlayer = async () => {
+  //     try {
+  //       await shaka.polyfill.installAll();
+  //       const player = new shaka.Player(videoRef.current);
+
+  //       player.configure({
+  //         controlPanelElements: [],
+  //       });
+
+  //       await player.load(videoUrl);
+
+  //       // TODO: There is no such function as `getTimeline()`
+
+  //       // Отримуємо елементи контролів
+  //       const controls = player.getControlsContainer();
+  //       if (controls) {
+  //         const timeline = controls.getTimeline();
+  //         timeline.addEventListener('mousemove', handleTimelineMouseMove);
+  //       }
+  //     } catch (error) {
+  //       console.error('error', error);
+  //     }
+  //   };
+
+  //   initPlayer();
+  // }, [videoUrl]);
 
   useEffect(() => {
     const player = new shaka.Player(videoRef.current);
@@ -27,9 +58,10 @@ const VideoPlayer: React.FC<Props> = ({ videoUrl }) => {
     player
       .load(videoUrl)
       .then(function () {
+        // This runs if the asynchronous load is successful.
         console.log('The video has now been loaded!');
       })
-      .catch((error) => console.error(error));
+      .catch((error) => console.error(error)); // onError is executed if the asynchronous load fails.
 
     controls?.setEnabledShakaControls(false);
 
@@ -39,39 +71,69 @@ const VideoPlayer: React.FC<Props> = ({ videoUrl }) => {
   }, [videoUrl]);
 
   const getDurationVideoMock = () => {
-    return videoRef.current?.duration || 0;
+    return 1;
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleMouseMove = (e: any) => {
+    const position =
+      (e.offsetX / e.target?.offsetWidth) * getDurationVideoMock();
+    const showSeekbarPreview = position * 1000;
+
+    console.log(showSeekbarPreview);
   };
 
   useEffect(() => {
-    const handleMetadataLoaded = () => {
-      const duration = getDurationVideoMock();
-      setVideoDuration(duration);
-    };
-
-    videoRef.current?.addEventListener('loadedmetadata', handleMetadataLoaded);
-
-    return () => {
-      videoRef.current?.removeEventListener(
-        'loadedmetadata',
-        handleMetadataLoaded
-      );
-    };
-  }, []);
-
-  const handleMouseMove = (event: Event) => {
-    // handleMouseMove...
-    // ...
-  };
+    const seekBar = videoRef.current?.querySelector('.shaka-seek-bar');
+    // load the thumbnail preview when the user moves over the seekbar
+    seekBar?.addEventListener('mousemove', handleMouseMove);
+  }, [handleMouseMove]);
 
   const timeFormat = (time: number) => {
-    //  timeFormat...
-    // ...
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes.toString().padStart(2, '0')}:${seconds
+      .toString()
+      .padStart(2, '0')}`;
+  };
+
+  const handleTimelineMouseMove = (event: MouseEvent) => {
+    const timeline = event.currentTarget as HTMLElement;
+    const rect = timeline.getBoundingClientRect();
+    const offsetX = event.clientX - rect.left;
+    const totalWidth = rect.width;
+    const percentage = offsetX / totalWidth;
+    const videoDuration = videoRef.current?.duration || 0;
+    const time = videoDuration * percentage;
+    setHoveredTime(time);
+    setIsHovering(true);
+
+    const canvasWidth = 100;
+    const canvasHeight = 50;
+    const canvasX = offsetX - canvasWidth / 2;
+    setCanvasX(canvasX);
+    const canvasY = rect.top - canvasHeight - 10;
+    previewRef.current!.style.left = `${canvasX}px`;
+    previewRef.current!.style.top = `${canvasY}px`;
   };
 
   return (
-    <div className={styles.videoContainer} ref={videoContainerRef}>
-      <div className={styles.previewСontainer}>
-        <canvas></canvas>
+    <div ref={videoContainerRef} className={styles.videoContainer}>
+      <div
+        className={styles.previewСontainer}
+        style={{
+          display: isHovering ? 'flex' : 'none',
+          left: `${canvasX + 50}px`,
+          top: 130 + 'px',
+        }}
+      >
+        <canvas
+          ref={previewRef}
+          width={100}
+          height={50}
+          className={styles.canvas}
+        />
+        <span className={styles.previewTime}>{timeFormat(hoveredTime)}</span>
       </div>
       <video
         ref={videoRef}
